a51e92b194d94a50b799ced8175eb1d5b6353947
karrmao <karrmao@gmail.com>
2022-10-26 20:18:57 +0300

    add

---
+++ less_5_webpack_intro/task2/webpack.config.js
@@ -0,0 +1,17 @@
+// const path = require('path');
+// const { mainModule } = require('process');
+
+// module.exports = {
+//   entry: {
+//     profile: './src/profile/index.js',
+//     dashboard: './src/dashboard/index.js',
+//   },
+//   output: {
+//     filename: '[name].js',
+//     path: path.join(__dirname, 'build'),
+//   },
+// };
+
+module.exports = {
+  entry: './src/profile/index.js',
+};
